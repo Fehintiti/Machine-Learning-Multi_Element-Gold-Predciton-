488602123df0f22770dcae96f978e4e62207a00b
Tomisin Okunlola <83586267+Fehintiti@users.noreply.github.com>
2024-05-02 10:05:46 +0100

    GoldPredict_app.py

--- GoldPredict_app.py
+++ GoldPredict_app.py
@@ -28,11 +28,13 @@ model = load(model_path)
 # Project Introduction
 st.header('Project Introduction')
 st.write("""
-The objective of this project is to develop a predictive model that can accurately determine the presence of gold in rock samples using historical multi-element assay data.
-Users are requested to enter the necessary input values into the designated fields and click the 'Predict' button to view the results.
+We aim to build a predictive model that can accurately predict the presence of gold in the rock samples based on the results of past historic Multi-element assay data.
+Please enter the required input values in the designated fields and press the 'Predict' button to see the results.
 """)
 
-# Instructions
+# Streamlit app layout
+st.title('Multi-Element Gold Prediction App')
+
 st.subheader('Instructions')
 st.write("""
 - Enter your input values in the following format: value1, value2, value3, ..., valueN.
@@ -41,26 +43,18 @@ st.write("""
 """)
 
 
-# Streamlit app layout
-st.title('Multi-Element Gold Prediction App')
-
 # Mapping of elements to their full names
 elements_full_names = {
-    'Al': 'Aluminum', 'As': 'Arsenic', 'Ag': 'Silver',  
-    'B': 'Boron', 'Ba': 'Barium', 'Be': 'Beryllium', 'Bi': 'Bismuth', 'Ca': 'Calcium',
-    'Cd': 'Cadmium', 'Co': 'Cobalt', 'Cr': 'Chromium', 'Cu': 'Copper', 'Fe': 'Iron',
-    'Ga': 'Gallium', 'Hg': 'Mercury', 'K': 'Potassium', 'La': 'Lanthanum', 'Mg': 'Magnesium',
-    'Mn': 'Manganese', 'Mo': 'Molybdenum', 'Na': 'Sodium', 'Ni': 'Nickel', 'P': 'Phosphorus',
-    'Pb': 'Lead', 'S': 'Sulfur', 'Sb': 'Antimony', 'Sc': 'Scandium', 'Sr': 'Strontium',
-    'Th': 'Thorium', 'Ti': 'Titanium', 'Tl': 'Thallium', 'V': 'Vanadium', 'W': 'Tungsten',
-    'Zn': 'Zinc', 'Zr': 'Zirconium'
+    'Al': 'Aluminum', 'As': 'Arsenic', 'B': 'Boron', 'Ba': 'Barium', 'Be': 'Beryllium', 'Bi': 'Bismuth', 'Ca': 'Calcium', 
+    'Cd': 'Cadmium', 'Co': 'Cobalt', 'Cr': 'Chromium', 'Cu': 'Copper', 'Fe': 'Iron', 'Ga': 'Gallium', 'Hg': 'Mercury', 
+    'K': 'Potassium', 'La': 'Lanthanum', 'Mg': 'Magnesium', 'Mn': 'Manganese', 'Mo': 'Molybdenum', 'Na': 'Sodium', 
+    'Ni': 'Nickel', 'P': 'Phosphorus', 'Pb': 'Lead', 'S': 'Sulfur', 'Sb': 'Antimony', 'Sc': 'Scandium', 'Sr': 'Strontium', 
+    'Th': 'Thorium', 'Ti': 'Titanium', 'Tl': 'Thallium', 'V': 'Vanadium', 'W': 'Tungsten', 'Zn': 'Zinc', 'Zr': 'Zirconium','Ag': 'Silver'
 }
-
-# Elements measured in percentage
 percentage_elements = ['Fe', 'Ca', 'Al', 'S', 'Ti']
 
 col1, col2, col3, col4, col5, col6, col7 = st.columns(7)
-
+# Creating columns for input
 # Creating columns for input
 inputs = {}
 for index, (symbol, full_name) in enumerate(elements_full_names.items()):
@@ -82,21 +76,27 @@ for index, (symbol, full_name) in enumerate(elements_full_names.items()):
     inputs[symbol] = col.text_input(f"{symbol} ({full_name}) in {unit}", key=f"{symbol}_key")
 
 if st.button('Predict'):
-    try:
-        # Collect input data and convert to float, treating blanks as zeros
-        input_list = [float(inputs[symbol]) if inputs[symbol] else 0.0 for symbol in elements_full_names.keys()]
-        new_data = np.array(input_list).reshape(1, -1)
+    input_list = []
+    error_detected = False
+    for symbol in elements_full_names.keys():
+        input_value = inputs[symbol].strip()
+        try:
+            # Convert to float, treating blanks as zeros
+            input_list.append(float(input_value) if input_value else 0.0)
+        except ValueError:
+            st.error(f"Invalid input for {symbol}: '{input_value}' is not a numeric value.")
+            error_detected = True
+            break
 
-        # Scale, PCA transform, and predict
-        new_data_scaled = scaler.transform(new_data)
-        new_data_pca = pca.transform(new_data_scaled)
-        prediction = model.predict(new_data_pca)
-
-        # Display the prediction
-        if prediction == 0:
-            st.write("No gold present")
-        else:
-            st.write("Gold present")
-    except ValueError:
-        st.error("Please ensure all inputs are numerical and appropriately formatted.")
-
+    if not error_detected:
+        try:
+            new_data = np.array(input_list).reshape(1, -1)
+            new_data_scaled = scaler.transform(new_data)
+            new_data_pca = pca.transform(new_data_scaled)
+            prediction = model.predict(new_data_pca)
+            if prediction == 0:
+                st.write("No gold present")
+            else:
+                st.write("Gold present")
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
